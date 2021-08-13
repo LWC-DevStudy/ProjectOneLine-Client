b44b0d9bd51faa39b846ce8c9761535d3bcbc48b
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-08-13 16:01:33 +0900

    🚑️ export / import 에러 수정

--- oneline-community/src/App.js
+++ oneline-community/src/App.js
@@ -1,29 +1,29 @@
 // library
 import React from 'react';
-import { Route } from 'react-router-dom';
-import { ConnectedRouter } from "connected-react-router";
-import { ThemeProvider } from 'styled-components';
+// import { Route } from 'react-router-dom';
+// import { ConnectedRouter } from 'connected-react-router';
+// import { ThemeProvider } from 'styled-components';
 
 // style
 
 // pages
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Write from './pages/Write';
+// import Home from './pages/Home';
+// import Login from './pages/Login';
+// import Signup from './pages/Signup';
+// import Write from './pages/Write';
 
 // components
-import Header from './components/Header';
+// import Header from './components/Header';
 
 function App() {
   return (
     // <ThemeProvider theme={theme}>
     <React.Fragment>
-      <Header />
+      {/* <Header />
       <Route path="/" exact component={Home} />
       <Route path="/signup" exact component={Signup} />
       <Route path="/login" exact component={Login} />
-      <Route path="/write" exact component={Write} />
+      <Route path="/write" exact component={Write} /> */}
     </React.Fragment>
   );
 }
