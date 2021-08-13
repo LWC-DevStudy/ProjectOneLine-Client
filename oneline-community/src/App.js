4337345151a0977b7ad9444f1de25eda9834d84e
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-08-13 15:53:12 +0900

    ✨폴더 구조 초기 작업

---
+++ oneline-community/src/App.js
@@ -0,0 +1,31 @@
+// library
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { ConnectedRouter } from "connected-react-router";
+import { ThemeProvider } from 'styled-components';
+
+// style
+
+// pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Write from './pages/Write';
+
+// components
+import Header from './components/Header';
+
+function App() {
+  return (
+    // <ThemeProvider theme={theme}>
+    <React.Fragment>
+      <Header />
+      <Route path="/" exact component={Home} />
+      <Route path="/signup" exact component={Signup} />
+      <Route path="/login" exact component={Login} />
+      <Route path="/write" exact component={Write} />
+    </React.Fragment>
+  );
+}
+
+export default App;
