1f58fc0b1e4b8185d8e69bb4ca212ed8cf923e03
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-08-13 16:46:20 +0900

    💄Grid, Text 작업 완료

---
+++ oneline-community/src/elements/Grid.js
@@ -0,0 +1,38 @@
+// library
+import React from 'react';
+import styled from 'styled-components';
+
+// style
+import { borderBox } from '../shared/style';
+
+const Grid = ({ children, ...props }) => {
+    return (
+        <React.Fragment>
+            <GridStyle {...props}>{children}</GridStyle>
+        </React.Fragment>
+    );
+};
+
+Grid.defalutProps = {
+    opacity: 1,
+    addstyle: () => {},
+};
+
+const GridStyle = styled.div`
+    width: ${(props) => 
+        props.width ? props.width : props.theme.size.defalutProps};
+    height: ${(props => 
+        props.height)};
+    background: ${(props) => 
+        props.bgColor && 
+        `rgba(${props.theme.palette[props.bgColor]}, ${props.opacity})`};
+    color: ${(props) => 
+        props.color &&
+        `rgba(${props.theme.palette[props.color]}, ${props.opacity})`};
+    margin: ${(props) => props.margin};
+    overflow: ${(props) => props.overflow};
+    ${(props) => borderBox(props.radius, props.padding)};
+    ${(props) => props.addstyle()};
+`;
+
+export default Grid;
