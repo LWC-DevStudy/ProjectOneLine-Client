d8a00c539c98d637739e2ef94d2cf623d92b3020
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-08-14 16:26:15 +0900

    ⚡️ Input label 추가 및 signUp연결

--- oneline-community/src/App.js
+++ oneline-community/src/App.js
@@ -10,7 +10,7 @@ import theme from './shared/style';
 // pages
 // import Home from './pages/Home';
 import Login from './pages/Login';
-// import Signup from './pages/Signup';
+import Signup from './pages/Signup';
 // import Write from './pages/Write';
 
 // components
@@ -22,10 +22,11 @@ function App() {
       <React.Fragment>
         {/* <Header />
       <Route path="/" exact component={Home} />
-      <Route path="/signup" exact component={Signup} />
+     
       <Route path="/login" exact component={Login} />
       <Route path="/write" exact component={Write} /> */}
         <Route path="/login" exact component={Login} />
+        <Route path="/signup" exact component={Signup} />
       </React.Fragment>
     </ThemeProvider>
   );
