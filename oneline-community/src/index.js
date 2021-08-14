3399d2e0305dada13358c4bb5a27f6114f7460aa
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-08-14 18:41:28 +0900

    ✨ 프로바이더 및 커넥티드라우터로 변경

--- oneline-community/src/index.js
+++ oneline-community/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store, { history } from './redux/configStore';
+import { ConnectedRouter } from 'connected-react-router';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
