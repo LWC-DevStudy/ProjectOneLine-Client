baf1164430494cbfdcd00fd690e3086bf39eebf8
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-08-14 18:01:46 +0900

    🚧 작성 페이지 중간 작업

--- oneline-community/src/pages/Home.js
+++ oneline-community/src/pages/Home.js
@@ -1,6 +1,7 @@
 // LIBRARY
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link,useLocation } from 'react-router-dom';
 
 // style
 import { borderBox, flexBox, flexHoz } from '../shared/style';
@@ -22,18 +23,20 @@ function Home() {
             <h1 style={{margin:'150px 0 -300px 0', fontSize:'60px', color:'white'}}>One line a day</h1>
                 
                 <Grid>
-                      <Button
-                          width="30%" 
-                          padding="12px" 
-                          margin="350px 0 0 350px"
-                          addstyle={() => {
-                              return css`
-                              display:block;
-                              `;
-                            }}
-                          >
-                          작성하기
-                      </Button>
+                    <Link to='/write' style={{textDecoration:'none'}}>
+                        <Button
+                            width="30%" 
+                            padding="12px" 
+                            margin="350px 0 0 350px"
+                            addstyle={() => {
+                                return css`
+                                display:block;
+                                `;
+                              }}
+                            >
+                            작성하기
+                        </Button>
+                    </Link>
 
                     <ContentsDiv/>
                     <ContentsDiv/>
