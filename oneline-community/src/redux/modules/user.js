2b15696e43e618daf7cfe5c7e1b0270d1bf6269a
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-08-16 03:48:03 +0900

    ✨ 로그인 구현

--- oneline-community/src/redux/modules/user.js
+++ oneline-community/src/redux/modules/user.js
@@ -5,6 +5,23 @@ import { createSlice } from '@reduxjs/toolkit';
 //token
 import { setToken, removeToken } from '../../shared/token.js';
 
+const initialState = {
+  user_info: { username: '정진우' },
+  is_login: false,
+};
+
+const user = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    SetUser: (state, action) => {
+      state.user_info.username = action.payload.username;
+      setToken('token', action.payload.token);
+      state.is_login = true;
+    },
+  },
+});
+
 //회원가입
 export const signUpDB =
   (user_info) =>
@@ -26,21 +43,16 @@ export const signUpDB =
   };
 
 //로그인
-export const loginDB =
+export const logInDB =
   (user_info) =>
   async (dispatch, getState, { history }) => {
     const username = user_info.username;
-    const password = user_info.passwordCheck;
+    const password = user_info.password;
     try {
       const login = await instance.post('/login', {
         username,
         password,
       });
-      const message = login.data.message;
-      if (message !== 'success') {
-        window.alert(login.data.message);
-        return;
-      }
       const login_info = {
         token: login.data.token,
         username: login.data.username,
@@ -52,23 +64,6 @@ export const loginDB =
       console.log(err);
     }
   };
-
-const initialState = {
-  user_info: { username: '정진우' },
-  is_login: false,
-};
-
-const user = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    SetUser: (state, action) => {
-      state.user_info.username = action.payload.username;
-      setToken('token', action.payload.token);
-      state.is_login = true;
-    },
-  },
-});
 
 export const { SetUser } = user.actions;
 
