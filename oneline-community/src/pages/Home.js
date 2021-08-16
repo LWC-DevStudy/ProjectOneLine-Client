fc3b5acfd637ad6a061aaf4eb0df99d134cb4674
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-08-16 16:29:58 +0900

    ✨ 글 불러오기 기능 구현

--- oneline-community/src/pages/Home.js
+++ oneline-community/src/pages/Home.js
@@ -1,7 +1,7 @@
 // LIBRARY
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link,useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 // style
 import { borderBox, flexBox, flexHoz } from '../shared/style';
@@ -16,50 +16,51 @@ import ContentsDiv from '../components/ContentsDiv';
 // REDUX
 import { getPostDB } from '../redux/modules/post';
 
-function Home() {
-    const dispatch = useDispatch();
-    const postList = useSelector((state) => state.post.list)
+function Home(props) {
+  const dispatch = useDispatch();
+  const postList = useSelector((state) => state.post.list);
+  console.log(postList);
+  React.useEffect(() => {
+    dispatch(getPostDB());
+  }, []);
+  return (
+    <div>
+      <Grid margin="0px auto 20px auto">
+        <h1
+          style={{
+            margin: '150px 0 -300px 0',
+            fontSize: '60px',
+            color: 'white',
+            textAlign: 'center',
+          }}
+        >
+          One line a day
+        </h1>
 
-    React.useEffect(() => {
-        dispatch(getPostDB());
-    },[])
-    return (
-        <div>
-            <Grid margin="0px auto 20px auto">
-            <h1 style={{
-              margin:'150px 0 -300px 0', 
-              fontSize:'60px', 
-              color:'white',
-              textAlign:'center'
-              }}>
-                One line a day
-            </h1>
-                
-                <Grid>
-                    <Link to='/write' style={{textDecoration:'none'}}>
-                        <Button
-                            width="30%" 
-                            padding="12px" 
-                            margin="350px 0 0 375px"
-                            addstyle={() => {
-                                return css`
-                                display:block;
-                                `;
-                              }}
-                            >
-                            작성하기
-                        </Button>
-                    </Link>
-                    <Grid margin="-120px 0 0 0">
-                        {postList.map((p, idx) => {
-                            return <ContentsDiv key={idx} {...p} />
-                        })}
-                    </Grid>
-                </Grid>
-                      
-            </Grid>
-        </div>
-    )
+        <Grid>
+          <Link to="/write" style={{ textDecoration: 'none' }}>
+            <Button
+              width="30%"
+              padding="12px"
+              margin="350px 0 0 375px"
+              addstyle={() => {
+                return css`
+                  display: block;
+                `;
+              }}
+            >
+              작성하기
+            </Button>
+          </Link>
+          <Grid margin="-120px 0 0 0">
+            {postList.map((p, idx) => {
+              return <ContentsDiv key={idx} {...p} />;
+            })}
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
