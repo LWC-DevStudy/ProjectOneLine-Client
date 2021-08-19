f18ceb7410d2e8aca975870df75928536d082e4b
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-08-19 14:11:41 +0900

    ⚡️ 작성하기 버튼 권한 부여, 내 게시글 표시

--- oneline-community/src/components/ContentsDiv.js
+++ oneline-community/src/components/ContentsDiv.js
@@ -10,6 +10,7 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 //ELEMENTS
 import { Grid, Text } from '../elements/index';
 import { likeToggleDB } from '../redux/modules/like';
+import { Button } from '@material-ui/core';
 
 function ContentsDiv(post) {
   const dispatch = useDispatch();
@@ -19,8 +20,65 @@ function ContentsDiv(post) {
   const updateLikes = () => {
     dispatch(likeToggleDB(postId));
   };
+  if (post.username === myPost) {
   return (
     <Grid
+      bgColor="yellow"
+      height="auto"
+      padding="10px 0 0 30px"
+      margin="30px 0 0 0"
+      addstyle={() => {
+        return css`
+          ${flexBox('space-between')}
+          position: relative;
+          top: 170px;
+          cursor: pointer;
+          
+        `;
+      }}
+    >
+      <Grid
+        padding="4px"
+        margin="4px"
+        clickEvent={() => {
+            history.push(`/detail/${post.postId}`);
+        }}
+      >
+        <Text width="400px" color="black" fontWeight="bold" fontSize="20px">
+          {post.contents}
+        </Text>
+      </Grid>
+
+      <Grid
+        addstyle={() => {
+          return css`
+            position: relative;
+            left: 40px;
+            bottom: 20px;
+            z-index: 1000;
+          `;
+        }}
+      >
+        <Grid>
+        My
+        </Grid>
+        <FavoriteIcon
+          onClick={updateLikes}
+          style={{
+            color: userLike ? 'red' : 'gray',
+            fontSize: '20px',
+            position: 'relative',
+            top: '3px',
+            marginRight: '5px',
+          }}
+        ></FavoriteIcon>
+        {post.likeCnt}
+        
+      </Grid>
+    </Grid>
+  );
+} return (
+<Grid
       bgColor="yellow"
       height="auto"
       padding="10px 0 0 30px"
@@ -38,11 +96,6 @@ function ContentsDiv(post) {
       <Grid
         padding="4px"
         margin="4px"
-        clickEvent={() => {
-          if (post.username === myPost) {
-            history.push(`/detail/${post.postId}`);
-          }
-        }}
       >
         <Text width="400px" color="black" fontWeight="bold" fontSize="20px">
           {post.contents}
@@ -74,5 +127,6 @@ function ContentsDiv(post) {
     </Grid>
   );
 }
+
 
 export default ContentsDiv;
